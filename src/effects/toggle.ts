aa35147f3165ffa62c5bc40af857f10c25dffc90
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2025-02-13 16:28:57 +0200

    Fixed `.show()` on the element with inline display:block and style tag

--- src/effects/toggle.ts
+++ src/effects/toggle.ts
@@ -17,20 +17,25 @@ fn.toggle = function ( this: Cash, force?: boolean ) {
     if ( !isElement ( ele ) ) return;
 
     const show = isUndefined ( force ) ? isHidden ( ele ) : force;
+    const display = ele.style.display;
 
     if ( show ) {
 
-      ele.style.display = ele[displayProperty] || '';
+      if ( !display || display === 'none' ) {
 
-      if ( isHidden ( ele ) ) {
+        ele.style.display = ele[displayProperty] || '';
 
-        ele.style.display = getDefaultDisplay ( ele.tagName );
+        if ( isHidden ( ele ) ) {
+
+          ele.style.display = getDefaultDisplay ( ele.tagName );
+
+        }
 
       }
 
-    } else if ( ele.style.display !== 'none' ) {
+    } else if ( display !== 'none' ) {
 
-      ele[displayProperty] = ele.style.display;
+      ele[displayProperty] = display;
 
       ele.style.display = 'none';
 
